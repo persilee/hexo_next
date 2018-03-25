70b6989bc5ce4844dd5e0449e82ba6dd30096f06
persilee <lishaoying@lishaoyingdeiMac.local>
2018-03-26 04:27:25 +0800

    完善第一遍博客

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,30 @@ var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 // 压缩 public 目录 css
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function () {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
         .pipe(gulp.dest('./public'));
 });
 // 压缩 public 目录 html
-gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
+gulp.task('minify-html', function () {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'))
 });
 // 压缩 public/js 目录 js
-gulp.task('minify-js', function() {
+gulp.task('minify-js', function () {
     return gulp.src('./public/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
 // 执行 gulp 命令时执行的任务
 gulp.task('default', [
-    'minify-html','minify-css','minify-js'
+    'minify-html', 'minify-css', 'minify-js'
 ]);
